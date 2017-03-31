aa10ad39592d915e7cfdafc43c3b65deeb790ca2
madeleineangergard <madeleine.angergard.6107@student.uu.se>
2017-03-31 13:56:29 +0200

    testar
    teastar

--- tapetap/cat/models.py
+++ tapetap/cat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from categories.models import CategoryBase
 
+class Category(models.Model)
+	name = models.CharField(max_length=128, unique=True)
+	def __unicode__(self):
+		return self.name
+class Page(models.Model)
+	category=models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+   	url = models.URLField()
+   	views = models.IntegerField(default=0)
+
+   	def __unicode__(self):
+       	return self.title
 # Create your models here.
