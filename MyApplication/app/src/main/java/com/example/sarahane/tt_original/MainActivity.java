c62ea9507ec5be504cd0559c327ccbae366f9857
MSkogsmo <markus.skogsmo.93@gmail.com>
2017-04-04 13:30:17 +0200

    Updates of paths and storage

--- MyApplication/app/src/main/java/com/example/sarahane/tt_original/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sarahane/tt_original/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.sarahane.tt_original;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creating path for storage by markusskogsmo
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
     }
 }
